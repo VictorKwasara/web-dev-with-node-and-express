3e1eb547a27616ea9f29f8c638226521701d69ad
victor_tk <victorkdevelopment@gmail.com>
2023-11-24 13:04:40 +0200

    created views and updated the routes to use them

--- meadowlark/app/meadowlark.js
+++ meadowlark/app/meadowlark.js
@@ -11,29 +11,25 @@ app.engine('handlebars', expressHandlebars({
 app.set('view engine', 'handlebars')
 
 app.get('/', (req,res) => {
-  res.type('text/plain')
-  res.send('Meadowlark Travel');
+  res.render('home')
 })
 
 app.get('/about', (req,res) => {
-  res.type('text/plain')
-  res.send('About Meadowlark Travel');
+  res.render('about')
 })
 
 
 // custom 404 page 
 app.use((req,res) => {
-  res.type('text/plain')
   res.status(404)
-  res.send('404 - Not Found')
+  res.render('404')
 })
 
 //custom 500 page 
 app.use((err,req,res,next) => {
   console.error(err.message)
-  res.type('text/plain')
   res.status(500)
-  res.send('500 - Server Error')
+  res.render('500')
 })
 
 app.listen(port, () => console.log(`Express started on http://localhost:${port}; press  Ctrl-C to terminate.`))
