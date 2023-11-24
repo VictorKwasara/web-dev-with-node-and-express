d8fe0031e56282c1572bb2a10d3a2e42b04da10e
victor_tk <victorkdevelopment@gmail.com>
2023-11-24 22:45:39 +0200

    Moved 'fortune cookie' into module.

--- meadowlark/app/meadowlark.js
+++ meadowlark/app/meadowlark.js
@@ -1,14 +1,8 @@
 const express = require('express')
+const fortune = require('./lib/fortume')
 const expressHandlebars = require('express-handlebars')
 const app = express()
 const port = process.env.PORT || 3000
-
-const fortunes = [
-"Conquer your fears or they will conquer you.",
-"Rivers need springs.",
-"Do not fear what you don't know.",
-"Whenever possible, keep it simple."
-]
 
 // configure Handlebars view engine 
 app.engine('handlebars', expressHandlebars({
@@ -16,27 +10,21 @@ app.engine('handlebars', expressHandlebars({
 }))
 app.use(express.static(`${__dirname}/public`))
 app.set('view engine', 'handlebars')
-
 app.get('/', (req,res) => {
   res.render('home')
 })
-
 app.get('/about', (req,res) => {
-  const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
-  res.render('about', {fortune: randomFortune})
+  res.render('about', {fortune: fortune.getFortune()})
 })
-
 // custom 404 page 
 app.use((req,res) => {
   res.status(404)
   res.render('404')
 })
-
 //custom 500 page 
 app.use((err,req,res,next) => {
   console.error(err.message)
   res.status(500)
   res.render('500')
 })
-
 app.listen(port, () => console.log(`Express started on http://localhost:${port}; press  Ctrl-C to terminate.`))
