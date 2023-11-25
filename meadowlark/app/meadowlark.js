5c98a47a04aa0fec6c86a76337ffe4e42fc9c055
victor_tk <victorkdevelopment@gmail.com>
2023-11-25 03:13:21 +0200

    test handlers with jest

--- meadowlark/app/meadowlark.js
+++ meadowlark/app/meadowlark.js
@@ -1,30 +1,18 @@
 const express = require('express')
-const fortune = require('./lib/fortume')
+const handlers = require('./lib/handlers')
 const expressHandlebars = require('express-handlebars')
 const app = express()
 const port = process.env.PORT || 3000
-
 // configure Handlebars view engine 
 app.engine('handlebars', expressHandlebars({
   defaultLayout: 'main',
 }))
 app.use(express.static(`${__dirname}/public`))
 app.set('view engine', 'handlebars')
-app.get('/', (req,res) => {
-  res.render('home')
-})
-app.get('/about', (req,res) => {
-  res.render('about', {fortune: fortune.getFortune()})
-})
+app.get('/', handlers.home)
+app.get('/about', handlers.about)
 // custom 404 page 
-app.use((req,res) => {
-  res.status(404)
-  res.render('404')
-})
+app.use(handlers.notFound)
 //custom 500 page 
-app.use((err,req,res,next) => {
-  console.error(err.message)
-  res.status(500)
-  res.render('500')
-})
+app.use(handlers.serverError)
 app.listen(port, () => console.log(`Express started on http://localhost:${port}; press  Ctrl-C to terminate.`))
